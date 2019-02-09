4af13caaec23df1db516ba474240762127c41c6f
Takuma <takuma@DESKTOP-4F2IIIP.localdomain>
2019-02-09 20:37:58 +0900

    export result images

--- devices/ocr.py
+++ devices/ocr.py
@@ -6,6 +6,29 @@ import sys
 import datetime
 import analogy_detector
 
+def view_results(path, save_dir, texts):
+    # 画像表示
+    im = Image.open(path)
+    (im_w, im_h) = im.size
+    draw = ImageDraw.Draw(im)
+    
+    print('Texts:')
+    for text in texts:
+
+        poly_xy =[] 
+        print('\n"{}"'.format(text.description))
+        vertices = (['({},{})'.format(vertex.x, vertex.y)
+                    for vertex in text.bounding_poly.vertices])
+        print('bounds: {}'.format(','.join(vertices)))
+        for vertex in text.bounding_poly.vertices:
+            print(' - ({}, {})'.format(vertex.x, vertex.y))
+            poly_xy.append((vertex.x, vertex.y))
+
+    # 画像保存
+    now = datetime.datetime.now()
+    save_path = save_dir + "result_" + now.strftime("%y%m%d%H%M%S") + ".jpg"
+    im.save(save_path)
+
 def recognize_text(path):
     client = vision.ImageAnnotatorClient()
     # road img
@@ -13,17 +36,14 @@ def recognize_text(path):
         content = image_file.read()
     image = vision.types.Image(content=content)
     texts = client.text_detection(image=image).text_annotations
-
-    print('Texts:')
-    for text in texts:
-        print('\n"{}"'.format(text.description))
-        vertices = (['({},{})'.format(vertex.x, vertex.y)
-                    for vertex in text.bounding_poly.vertices])
-        print('bounds: {}'.format(','.join(vertices)))
+        
+    return texts
 
 if __name__ == '__main__':
-    if len(sys.argv) < 2:
-        print("Err : image_file is not set.")
+    if len(sys.argv) < 3:
+        print("Err : image_file, and save_dir is not set.")
         sys.exit(1)
     path = sys.argv[1]
-    recognize_text(path)
+    save_dir = sys.argv[2]
+    texts = recognize_text(path)
+    view_results(path, save_dir, texts)
